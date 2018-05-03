e74f18313f1148acf0dcc617f46caa4b5d24ebe4
Tayte Stokes <taytestokes9328@gmail.com>
2018-05-03 09:01:10 -0600

    Made progress on the assignment

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 export default class ClassList extends Component {
   constructor() {
     super()
     
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount(props){
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then( (response) => {
+      this.setState({students: response.data})
+    })
   }
 
   render() {
+    let mappedStudents = this.state.students.map((element, index) => {
+      return(
+       <Link to={`/student/${element.id}`} key={element.index}><h3 key={element.index}>{element.first_name}{element.last_name}</h3></Link>
+      )
+    })
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+        {mappedStudents}
       </div>
     )
   }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div className="box">
-        <button className='btn'>Math 1010</button>
-        <button className='btn'>English 2010</button>
-        <button className='btn'>Biology 2020</button>
+        <Link to="MATH1010" className='btn'>Math 1010</Link>
+        <Link to="ENG2010" className='btn'>English 2010</Link>
+        <Link to="BIO2020" className='btn'>Biology 2020</Link>
       </div>
     );
   }
